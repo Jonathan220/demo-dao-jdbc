35ce856bd017b9232e2fd8191f94ffe77271ac7b
jonathan220 <abreu.jonathan220@gmail.com>
2024-03-11 08:54:48 -0300

    Department entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department departamento = new Department(1, "books");
+        System.out.println(departamento);
     }
 }
