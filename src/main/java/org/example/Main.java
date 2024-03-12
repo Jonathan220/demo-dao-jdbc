85359206337d99d729cdf2cbd6166430ca2ea90e
jonathan220 <abreu.jonathan220@gmail.com>
2024-03-12 08:46:56 -0300

    SellerDaoJDBC and DaoFactory

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.model.dao.DaoFactory;
+import org.example.model.dao.SellerDao;
 import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Main {
         Department departamento = new Department(1, "books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departamento);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
