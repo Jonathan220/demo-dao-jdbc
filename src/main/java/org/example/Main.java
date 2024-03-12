1753c9e1b6776410d5f27cc7741b702c697577c9
jonathan220 <abreu.jonathan220@gmail.com>
2024-03-12 08:01:05 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department departamento = new Department(1, "books");
-        System.out.println(departamento);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departamento);
+        System.out.println(seller);
     }
 }
